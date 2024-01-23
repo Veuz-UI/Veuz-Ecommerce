be5605268d621ee2babd45a462c41b41264e6f9f
anakha <anakha@veuz.in>
2024-01-23 18:04:39 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,154 @@
+<footer class="main">
+        <section class="newsletter mb-15 wow animate__animated animate__fadeIn">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="position-relative newsletter-inner">
+                            <div class="newsletter-content text-center">
+                                <h5>Big Saving on Top selling Smartphone</h5>
+                                <h4>Get Up To 85% OFF on big billion day 2021</h4>
+                                <a href="#" class="shopnow">Shop Now</a>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section-padding pb-5 onsal">
+            <div class="container">
+                <div class="section-title wow animate__animated animate__fadeIn" data-wow-delay="0">
+                    <h3 class="">OUR PARTNERS</h3>
+
+                </div>
+                <hr>
+                <section class="customer-logos slider">
+                    <div class="slide"><img src="assets/imgs/client/partner1.svg"></div>
+                    <div class="slide"><img src="assets/imgs/client/partner2.svg"></div>
+                    <div class="slide"><img src="assets/imgs/client/partner3.svg">
+                    </div>
+                    <div class="slide"><img src="assets/imgs/client/partner4.svg">
+                    </div>
+                    <div class="slide"><img src="assets/imgs/client/partner5.svg"></div>
+                    <div class="slide"><img src="assets/imgs/client/partner1.svg"></div>
+                    <div class="slide"><img src="assets/imgs/client/partner2.svg">
+                    </div>
+                    <div class="slide"><img src="assets/imgs/client/partner3.svg">
+                    </div>
+                    <div class="slide"><img src="assets/imgs/client/partner4.svg">
+                    </div>
+                    <div class="slide"><img src="assets/imgs/client/partner5.svg"></div>
+                </section>
+            </div>
+        </section>
+        <div class="newsltr text-center">
+            <h2>Subscribe Our Newsletter</h2>
+            <form class="form-subcriber d-flex">
+                <input type="email" placeholder="Domain@gmail.com">
+                <button class="btn" type="submit">Subscribe</button>
+            </form>
+        </div>
+
+        <section class="section-padding footer-mid">
+            <div class="container pt-15">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="widget-about font-md mb-md-3 mb-lg-3 mb-xl-0 wow animate__animated animate__fadeInUp"
+                            data-wow-delay="0">
+                            <div class="logo mb-30">
+                                <a href="index.html" class="mb-15"><img src="assets/imgs/theme/log.jpg"
+                                        alt="logo" /></a>
+                                <p class="font-lg text-heading">Lorem Ipsum is simply dummy text of the printing and
+                                    typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever
+                                    since the 1500s, when an unknown printer took a galley of type and scrambled it to
+                                    make a type specimen book.</p>
+                                <div class="mobile-social-icon">
+
+                                    <a href="#"><img src="assets/imgs/icons/facebook.svg" alt=""></a>
+                                    <a href="#"><img src="assets/imgs/icons/twitter.svg" alt=""></a>
+                                    <a href="#"><img src="assets/imgs/icons/linkedin.svg" alt=""></a>
+                                    <a href="#"><img src="assets/imgs/icons/insta.svg" alt=""></a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="footer-link-widget col wow animate__animated animate__fadeInUp" data-wow-delay=".2s">
+                        <h4 class="widget-title">First Menu</h4>
+                        <ul class="footer-list mb-sm-5 mb-md-0">
+                            <li><a href="#">Gifts</a></li>
+                            <li><a href="#">Enterpise</a></li>
+                            <li><a href="#">Security</a></li>
+                            <li><a href="#">Customer Stories</a></li>
+
+                        </ul>
+                    </div>
+                    <div class="footer-link-widget col wow animate__animated animate__fadeInUp" data-wow-delay=".3s">
+                        <h4 class="widget-title">First Menu</h4>
+                        <ul class="footer-list mb-sm-5 mb-md-0">
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Enterpise</a></li>
+                            <li><a href="#">Security</a></li>
+                            <li><a href="#">Customer Stories</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-link-widget col wow animate__animated animate__fadeInUp" data-wow-delay=".4s">
+                        <h4 class="widget-title">First Menu</h4>
+                        <ul class="footer-list mb-sm-5 mb-md-0">
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Enterpise</a></li>
+                            <li><a href="#">Security</a></li>
+                            <li><a href="#">Customer Stories</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-link-widget widget-install-app col wow animate__animated animate__fadeInUp"
+                        data-wow-delay=".5s">
+                        <h4 class="widget-title">Download Now !</h4>
+
+                        <div class="download-app">
+                            <a href="#" class="hover-up mb-sm-2 mb-lg-0"><img class="active"
+                                    src="assets/imgs/theme/app-store.svg" alt="" /></a>
+                            <a href="#" class="hover-up mb-sm-2"><img src="assets/imgs/theme/googleply.svg"
+                                    alt="" /></a>
+
+                            <p class="mb-20">Secured Payment Gateways</p>
+                            <ul>
+
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="assets/imgs/theme/1.jpg" class="img-fluid blur-up lazyloaded"
+                                            alt="payment icon"></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="assets/imgs/theme/2.jpg" class="img-fluid blur-up lazyloaded"
+                                            alt="payment icon"></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="assets/imgs/theme/3.jpg" class="img-fluid blur-up lazyloaded"
+                                            alt="payment icon"></a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0)">
+                                        <img src="assets/imgs/theme/4.jpg" class="img-fluid blur-up lazyloaded"
+                                            alt="payment icon"></a>
+                                </li>
+                            </ul>
+                        </div>
+
+
+                    </div>
+                </div>
+        </section>
+        <div class="container pb-30 wow animate__animated animate__fadeInUp copyrght" data-wow-delay="0">
+            <div class="row align-items-center">
+                <hr>
+                <div class="col-xl-12 col-lg-12 col-md-12 text-center">
+                    <p class="font-sm mb-0">©2021 veuzconcepts.com All Rights Reserved</p>
+                </div>
+
+            </div>
+        </div>
+    </footer>
---
+++ product-details.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Veuz</title>
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:title" content="" />
+    <meta property="og:type" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/imgs/favicon.ico" />
+    <!-- Template CSS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/vendors/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/animate.min.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+
+    <link rel='stylesheet' href='https://sachinchoolur.github.io/lightslider/dist/css/lightslider.css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+<?php include 'header.php';?>
+<main class="main">
+    <div class="container">
+        <!-- brudcrumbs -->
+        <div class="common-v-padding">
+<ul class="breadcrumb-list">
+    <li>  <a href="#">Home Page</a>
+        <span class="separator" aria-hidden="true">&rsaquo;</span></li>
+    <li>
+        <a href="#">Cloths</a>
+        <span class="separator" aria-hidden="true">&rsaquo;</span>
+    </li>
+    <li class="active-brudcrumb">
+        <a href="#">Armani T-shirt New Black Color 2024 Model</a>
+    </li>
+</ul>
+
+
+<!-- product slider section -->
+<div class="row">
+<div class="col-md-5">
+
+
+    <ul id="lightSlider">
+      <li data-thumb="https://i.imgur.com/KZpuufK.jpg"> <img src="https://i.imgur.com/KZpuufK.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/GwiUmQA.jpg"> <img src="https://i.imgur.com/GwiUmQA.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/DhKkTrG.jpg"> <img src="https://i.imgur.com/DhKkTrG.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/kYWqL7k.jpg"> <img src="https://i.imgur.com/kYWqL7k.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/c9uUysL.jpg"> <img src="https://i.imgur.com/c9uUysL.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/KZpuufK.jpg"> <img src="https://i.imgur.com/KZpuufK.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/GwiUmQA.jpg"> <img src="https://i.imgur.com/GwiUmQA.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/DhKkTrG.jpg"> <img src="https://i.imgur.com/DhKkTrG.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/kYWqL7k.jpg"> <img src="https://i.imgur.com/kYWqL7k.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/c9uUysL.jpg"> <img src="https://i.imgur.com/c9uUysL.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/KZpuufK.jpg"> <img src="https://i.imgur.com/KZpuufK.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/GwiUmQA.jpg"> <img src="https://i.imgur.com/GwiUmQA.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/DhKkTrG.jpg"> <img src="https://i.imgur.com/DhKkTrG.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/kYWqL7k.jpg"> <img src="https://i.imgur.com/kYWqL7k.jpg" /> </li>
+      <li data-thumb="https://i.imgur.com/c9uUysL.jpg"> <img src="https://i.imgur.com/c9uUysL.jpg" /> </li>
+    </ul>
+    
+  </div>
+
+  <div class="col-lg-7">
+    <div class="">
+<h3>Armani T-shirt New Black Color 2024 Model</h3>
+
+<div class="d-flex align-items-center">
+<div>
+    <span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star"></span>
+<span class="fa fa-star"></span>
+</div>
+<p>(21 Ratings) 36 Months' Warranty</p>
+</div>
+
+<h3>SAR 250<span>(50%OFF)</span></h3>
+    </div>
+  </div>
+
+</div>
+</div>
+    </div>
+</main>
+
+<?php include 'footer.php';?>
+    <!-- Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="text-center">
+                    <img src="assets/imgs/theme/loading.gif" alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Vendor JS-->
+
+    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/plugins/slick.js"></script>
+    <script src="assets/js/plugins/wow.js"></script>
+    <script src="assets/js/plugins/magnific-popup.js"></script>
+<script src='https://sachinchoolur.github.io/lightslider/dist/js/lightslider.js'></script>
+
+    <!-- Template  JS -->
+    <script src="assets/js/script.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('.customer-logos').slick({
+                slidesToShow: 8,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 1500,
+                arrows: false,
+                dots: false,
+                pauseOnHover: false,
+                responsive: [{
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 4
+                    }
+                }, {
+                    breakpoint: 520,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                }]
+            });
+        });
+    </script>
+
+
+
+
+<script>
+  $('#lightSlider').lightSlider({
+    gallery: true,
+    item: 1,
+    loop: true,
+    slideMargin: 0,
+    thumbItem: 5
+  });
+</script>
+</body>
+
+</html>
