240c1e46bc29ece3802715b8f6f1946050d03337
anakha <anakha@veuz.in>
2024-02-09 10:35:40 +0530

    responsive-last

--- footer.php
+++ footer.php
@@ -53,7 +53,7 @@
         <section class="section-padding footer-mid">
             <div class="container pt-15">
                 <div class="row">
-                    <div class="col-xl-3">
+                    <div class="col-xl-3 col-lg-5">
                         <div class="widget-about font-md mb-md-3 mb-lg-3 mb-xl-0 wow animate__animated animate__fadeInUp"
                             data-wow-delay="0">
                             <div class="logo mb-30">
@@ -63,8 +63,8 @@
                                     typesetting industry.</p>
                                     <ul class="contact-infor">
                                 <li><img src="assets/imgs/theme/icons/icon-location.svg" alt=""><strong>Address: </strong> <span> Riyadh, Jeddah, Al Khobar Bahrain, Qatar</span></li>
-                                <li><img src="assets/imgs/theme/icons/icon-contact.svg" alt=""><strong>Call Us:</strong><span> (+91) - 9876-124553</span></li>
-                                <li><img src="assets/imgs/theme/icons/icon-email-2.svg" alt=""><strong>Email:</strong><span> sales@veuz.com</span></li>
+                                <li><img src="assets/imgs/theme/icons/icon-contact.svg" alt=""><strong>Call Us:</strong><span><a href="tel:+919876124553">(+91) - 9876-124553</a> </span></li>
+                                <li><img src="assets/imgs/theme/icons/icon-email-2.svg" alt=""><strong>Email:</strong><span> <a href="mailto:sales@veuz.com">sales@veuz.com</a></span></li>
                                 <li><img src="assets/imgs/theme/icons/icon-clock.svg" alt=""><strong>Hours:</strong><span> 10:00 - 18:00, Mon - Sat</span></li>
                             </ul>
                             </div>
@@ -93,7 +93,7 @@
                             <li><a href="#">Order History</a></li>
                         </ul>
                     </div>
-                    <div class="footer-link-widget col wow animate__animated animate__fadeInUp" data-wow-delay=".4s">
+                    <div class="footer-link-widget col wow animate__animated animate__fadeInUp ml-0" data-wow-delay=".4s">
                         <h4 class="widget-title">Consumer Policy</h4>
                         <ul class="footer-list mb-sm-5 mb-md-0">
                             <li><a href="#">Features</a></li>
@@ -105,13 +105,22 @@
                     
                     <div class="footer-link-widget widget-install-app col wow animate__animated animate__fadeInUp"
                         data-wow-delay=".5s">
-                        <h4 class="widget-title">Download Now !</h4>
+                        <h4 class="widget-title">Install App</h4>
+                        <p class="font-lg text-heading mb-3">Get 10% off  your first order !</p>
 
                         <div class="download-app">
-                            <a href="#" class="hover-up mb-sm-2 mb-lg-0"><img class="active"
-                                    src="assets/imgs/theme/app-store.svg" alt="" /></a>
-                            <a href="#" class="hover-up mb-sm-2"><img src="assets/imgs/theme/googleply.svg"
-                                    alt="" /></a>
+                         
+                            <div class="apps">
+                                <img src="assets/imgs/icons/qr.png" alt="" class="qr-img">
+                                <div class="app-imgs">
+                                    <a href="#" class="hover-up"><img class="active"
+                                        src="assets/imgs/theme/app-store.svg" alt="" /></a>
+                                    <a href="#" class="hover-up"><img src="assets/imgs/theme/googleply.svg"
+                                        alt="" /></a>
+                                </div>
+                         
+                            </div>
+                          
 
                             <p class="mb-20">Secured Payment Gateways</p>
                             <ul>
@@ -146,13 +155,13 @@
     
         <div class="container pb-30 wow animate__animated animate__fadeInUp copyrght" data-wow-delay="0">
             <div class="row align-items-center">
-                <div class="col-12 mb-30">
+                <div class="col-12 mb-25">
                     <div class="footer-bottom"></div>
                 </div>
                 <div class="col-xl-4 col-lg-6 col-md-6">
                     <p class="font-sm mb-0">Copyright 2024, <strong class="text-brand">Veuz</strong> - Ecommerce <br>All rights reserved</p>
                 </div>
-                <div class="col-xl-4 col-lg-6 text-center d-none d-xl-block">
+                <div class="col-xl-4 col-lg-6 text-center d-lg-none d-xl-block">
                     <div class="hotline d-lg-inline-flex mr-30">
                         <img src="assets/imgs/theme/icons/phone-call.svg" alt="hotline">
                         <p>1036-0369<span>Working 8:00 - 22:00</span></p>
